bda333a4d715e83e593ef477d1a95f36ef8f311a
Felix-Neko <felix-neko@example.com>
2026-08-06 21:21:23 +0300

    Мелкие правки...

--- run_detect_and_crop_mts_1.sh
+++ run_detect_and_crop_mts_1.sh
@@ -8,19 +8,18 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-INPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/в работе/1966-1976 (300 DPI)"
-OUTPUT_DIR="/mnt/system/raw/mts/iter_12/cropped"
-DEBUG_DIR="/mnt/system/raw/mts/iter_12/debug"
+INPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/в работе/пак-1 1966-1976 (300 DPI)"
+OUTPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/нарезка сканов/пак-1 (1966-1976)/cropped"
+#DEBUG_DIR="/mnt/system/raw/mts/iter_12/debug"
 
 echo "detect_and_crop:"
 echo "  input  = $INPUT_DIR"
 echo "  output = $OUTPUT_DIR"
-echo "  debug  = $DEBUG_DIR"
+#echo "  debug  = $DEBUG_DIR"
 
 uv run python -m ocr_utils.scan_cropping \
     --input-dir "$INPUT_DIR" \
     --output-dir "$OUTPUT_DIR" \
-    --debug-dir "$DEBUG_DIR" \
     --recursive \
     --top-margin -120 \
     --bottom-margin -180 \
@@ -43,5 +42,4 @@ uv run python -m ocr_utils.scan_cropping \
     --crop-mode=pixel-exact \
     --crop-fill-method=replicate \
     --crop-fill-blur-px=32 \
-    --crop-fill-fade=0.0 \
-
+    --crop-fill-fade=0.0
--- run_detect_and_crop_ve_k48_pack3.sh
+++ run_detect_and_crop_ve_k48_pack3.sh
@@ -9,9 +9,19 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 #INPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/в работе"
-INPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/в работе/досканы-1 1970-07 - 1970-09 (450 DPI)/"
-OUTPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/нарезка сканов/досканы-1 1970-07 - 1970-09 (450 DPI)/v3/cropped"
-DEBUG_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/МТС/нарезка сканов/досканы-1 1970-07 - 1970-09 (450 DPI)/v3/debug"
+#INPUT_DIR="/mnt/system/raw/плохие сканы  ВЭ/06"
+#INPUT_DIR="/mnt/system/raw/ve_80s/in"
+#INPUT_DIR="/mnt/system/raw/ve_80s/in/1989/06 проверить зональный пересвет"
+#OUTPUT_DIR="/mnt/system/raw/ve_80s/test_896_tiff_9/out"
+#DEBUG_DIR="/mnt/system/raw/ve_80s/test_896_tiff_9/debug"
+
+#INPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/Вопросы экономики/в работе/пак-2 (450 DPI)"
+#OUTPUT_DIR="/mnt/system/raw/ve_test/pack2/iter_1/cropped"
+#DEBUG_DIR="/mnt/system/raw/ve_test/pack2/iter_1/debug"
+
+INPUT_DIR="/mnt/dump3/yandex_disk_linux_baby_zergling/Общее/Фотки/Вопросы экономики/в работе/пак-3 (450 DPI улучшили фокус)"
+OUTPUT_DIR="/mnt/SYSTEM/raw/ve/pack_3/v_1/cropped"
+DEBUG_DIR="/mnt/SYSTEM/raw/ve/pack_3/v_1/debug"
 
 echo "detect_and_crop:"
 echo "  input  = $INPUT_DIR"
@@ -23,17 +33,17 @@ uv run python -m ocr_utils.scan_cropping \
     --output-dir "$OUTPUT_DIR" \
     --debug-dir "$DEBUG_DIR" \
     --recursive \
-    --top-margin -180 \
+    --top-margin -190 \
     --bottom-margin -270 \
-    --left-margin -360 \
-    --right-margin -360 \
+    --left-margin -300 \
+    --right-margin -300 \
     --output-format tiff \
     --force-dpi=450 \
     --compensate-levels \
-  --finger-dilate-px=120 \
-    --max-asymmetric-dilation-ratio=1. \
-    --finger-zone-light-increment=10,10 \
-    --extra-erosion-px=120 \
+    --finger-dilate-px=110 \
+    --max-asymmetric-dilation-ratio=1.6 \
+    --finger-zone-light-increment=20,20 \
+    --extra-erosion-px=80 \
     --protect-text-layout \
     --text-protect-mode=copy-back-layout-zones \
     --layout-pad-px=18,72 \
@@ -43,6 +53,6 @@ uv run python -m ocr_utils.scan_cropping \
     --remove-fingers \
     --crop-mode=pixel-exact \
     --crop-fill-method=replicate \
-    --crop-fill-blur-px=32 \
+    --crop-fill-blur-px=48 \
     --crop-fill-fade=0.0 \
 
